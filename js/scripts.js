d26eb3682e04ce004a5dd16a34a239ff8bca4eee
Ami Cooper and Kelsey guerra <ami@>
2018-03-20 14:41:15 -0700

    add randomized animal sounds and css

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//Create clickable buttons on site which prepend prewritten cat and dog sounds.
+$(document).ready(function() {
+  $("button#Dogs").click(function() {
+    $("ul#left").append("<li>"+dogSound+"</li>");
+    });
+
+  $("button#Cats").click(function() {
+    $("ul#right").append("<li>" +catSound+"</li>");
+  });
+  //Create arrays for dog and cat sounds to be randomized
+  var dogArray = [
+    "Bark!", "Woof!", "Arf!", "Grrr!"
+  ];
+  var catArray = [
+    "Meow!", "Mew!", "Purr!", "Hiss!"
+  ];
+var dogSound = dogArray[
+  Math.floor(Math.random()*dogArray.length)];
+var catSound = catArray[
+  Math.floor(Math.random()*catArray.length)
+];
+});
